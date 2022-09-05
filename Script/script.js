72d8bd8a78d04dd1d849b1a441cc17b408b096f6
lukasnogueira <79331207+lukasnogueira@users.noreply.github.com>
2022-09-05 18:12:30 -0300

    processo de conclusão do style e validação js

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,32 @@
-function valida_envia() {
+function valida_envia(form) {
+    let validation = ['name', 'email', 'data_nascimento', 'naturalidade', 'empresa', 'cargo', 'funcao', 'Comp_obj', 'instituicao']
+    let validaradius = ['masculino', 'feminino', 'pfnd']
+
+    let sexo = ''
+    let msg = ''
+
+    console.log('0')
     
+    for ( i in validation) {
+        if (document.querySelector(`#${validation[i]}`).value == '') {
+            msg += 'Faltou preencher o campo' + ' ' + validation[i] + '\n'
+        }
+    }
+
+   for (j in validaradius) {
+        if (!$("input[name='sexo']:checked").length) {
+            sexo = 'ok'
+        }
+        console.log(document.querySelector(`#${validaradius}`).checked)
+    }
+    
+
+    
+    if (sexo == '' ) {
+        msg += 'Faltou selecionar o Genero.' + '\n'
+    }
+
+    if (msg != '') {
+        alert(msg)
+    }
 }
